edea795d84c3c7687a1ff97031e631ceb76feae2
Kirill <k.dudarev96@gmail.com>
2022-03-26 10:05:30 +0300

    Added the ability to create new tasks, some fixes

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, TaskDetailView
+from .views import HomeView, TaskDetailView, AddTaskView
 
 
 app_name = 'tasks'
@@ -8,4 +8,5 @@ app_name = 'tasks'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('add_task/', AddTaskView.as_view(), name='add_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
@@ -12,3 +14,14 @@ class HomeView(ListView):
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'tasks/task_detail.html'
+
+
+class AddTaskView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/add_task.html'
+    success_url = reverse_lazy('tasks:home')
+
+    def form_valid(self, form):
+        form.instance.reporter = self.request.user
+        return super(AddTaskView, self).form_valid(form)
