4808056c6517fc0015be8240ffc42dacac5d5ba8
Kirill <k.dudarev96@gmail.com>
2022-03-26 09:29:15 +0300

    Added homepage and task detail page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .views import HomeView, TaskDetailView
+
 
 app_name = 'tasks'
 
 urlpatterns = [
-
+    path('', HomeView.as_view(), name='home'),
+    path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from tasks.models import Task
+
+
+class HomeView(ListView):
+    model = Task
+    template_name = 'tasks/home.html'
+    ordering = ['-date_added']
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
